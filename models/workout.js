77cdae966b489fea4d5687bf6165c9aaf3f62edd
Zo Beki <uobeki.40@gmail.com>
2021-04-20 19:14:29 -0400

    models update

--- models/workout.js
+++ models/workout.js
@@ -39,19 +39,9 @@ const workoutSchema = new Schema(
                 }
             }
         ]
-    },
-    {
-        toJSON: {
-            virtuals: true
-        }
     }
 );
 
-workoutSchema.virtual("totalDuration").get(function () {
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
-});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
